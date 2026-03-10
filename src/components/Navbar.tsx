83ab974c4a2b9cfb66d044dc09f4834952a3ab04
Sufi Inam Ul Hassan <inamulhassan.tech@gmail.com>
2026-03-10 15:07:41 +0500

    Fix import path for ScrollSmoother in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { MdMenu, MdClose } from "react-icons/md";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import HoverLinks from "./HoverLinks";
 import { gsap } from "gsap";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 import "./styles/Navbar.css";
 
 gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
